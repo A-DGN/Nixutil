0a2865bdd20d6407b72f9c496506eaac7b2d2621
adrien.daugan <adrien.daugan@qonto.com>
2024-08-02 16:35:47 +0200

    commit

---
+++ archived/main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Fonction pour afficher le menu
+show_menu() {
+    clear
+    echo "==== Mon TUI Simple ===="
+    echo "1. Option 1"
+    echo "2. Option 2"
+    echo "3. Option 3"
+    echo "4. Quitter"
+    echo "========================"
+    echo "Choisissez une option : "
+}
