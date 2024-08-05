6e34e2f9b42ddfa54b801a97f3adb1a10e480fb7
adrien.daugan <adrien.daugan@qonto.com>
2024-08-05 14:21:05 +0200

    update nixinstaller

--- src/backend/nixinstaller_protocol/iso.nix
+++ src/backend/nixinstaller_protocol/iso.nix
@@ -1,8 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  # Configuration pour une image ISO minimale
   imports = [
-    ./configuration.nix # votre configuration de base pour NixOS
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
   ];
 
   # Configuration pour démarrer automatiquement le script d'installation
@@ -12,10 +13,18 @@
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.wget}/bin/wget -O /tmp/install.sh <URL_de_votre_script>";
-      ExecStartPost = "${pkgs.bash}/bin/bash /tmp/install.sh";
+      ExecStart = "${pkgs.wget}/bin/wget -O /tmp/nixinstaller.sh https://github.com/A-DGN/Nixutil/blob/main/src/backend/nixinstaller_protocol/nixinstaller.sh";
+      ExecStartPost = "${pkgs.bash}/bin/bash /tmp/nixinstaller.sh";
       Type = "oneshot";
       RemainAfterExit = true;
     };
   };
 }
+
+
+# Get nixos specific version
+# nix-channel --add https://channels.nixos.org/nixos-24.05 nixos-24.05 && nix-channel --update
+
+
+# Build the ISO with a specific version
+# nix-build '<nixos-24.05/nixos>' -A config.system.build.isoImage -I nixos-config=./iso.nix
