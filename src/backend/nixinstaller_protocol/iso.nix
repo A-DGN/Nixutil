1eae830d464b95cc34f5b28ec1923a9b2f5ee759
adrien.daugan <adrien.daugan@qonto.com>
2024-08-05 18:04:00 +0200

    last edit

--- src/backend/nixinstaller_protocol/iso.nix
+++ src/backend/nixinstaller_protocol/iso.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  # Configuration pour une image ISO minimale
+  # Config for nixos minimal
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
   ];
 
-  # Configuration pour démarrer automatiquement le script d'installation
+  # Setup for downloading the setup script and execute it
   systemd.services.install-script = {
     enable = true;
     description = "Télécharge et exécute le script d'installation";
@@ -22,9 +22,13 @@
 }
 
 
-# Get nixos specific version
+# 1 - Get nixos specific version
 # nix-channel --add https://channels.nixos.org/nixos-24.05 nixos-24.05 && nix-channel --update
 
 
-# Build the ISO with a specific version
+# 2 - Build the ISO with a specific version
 # nix-build '<nixos-24.05/nixos>' -A config.system.build.isoImage -I nixos-config=./iso.nix
+
+# 3 - Send you the iso
+# scp -r root@90.90.138.199:folder_path reception_path
+
